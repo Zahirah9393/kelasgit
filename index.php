c08cedea4955349ab907003bf9a0f4cbd01c660f
Zahirah <Zahirah@k4ml.github.io>
2015-08-16 14:47:28 +0800

    balas

--- index.php
+++ index.php
@@ -8,4 +8,4 @@ echo "Hi, I'm Zahirah";
 
 echo "master is your repository's main branch and should be the one true source of your code's production-ready version. If the end product you are creating is live to users, that code should be in the master branch. Alternative versions (ones you are working on, features you are adding and testing locally/on a staging or beta version etc) have their code changes in a separate branch by other names, and you merge that in to your master branch when it's ready to be live.";
 
-echo "sdfgdfgdf";
+echo "Hehe, ngah layan syira";
